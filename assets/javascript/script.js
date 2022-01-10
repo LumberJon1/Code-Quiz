9a4a12289be396cdc541502c8bcf351e0cc0a257
Jonathan W <jwilliams12117@gmail.com>
2022-01-09 19:41:23 -0700

    Added event handler and function to start screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,20 @@
 //Variables for the intro page logic
-
+var introSectionEl = document.querySelector("#intro-section");
+var introTitleEl = introSectionEl.querySelector("h1");
+var introDescriptionEl = introSectionEl.querySelector("p");
+var startButton = introSectionEl.querySelector("button");
 
 //Variables for the quiz element logic
 
-//Function to add intro page
+//Function to toggle the display of the intro page
+var toggleIntro = function() {
+    if (introSectionEl.style.display === "none") {
+        introSectionEl.style.display = "flex";    
+    }
+    else {
+        introSectionEl.style.display = "none";
+    }
+}
 
 //Function to add quiz elements to the page
 
@@ -12,5 +23,6 @@
 //Function to decrement the time/score
 
 //Event listeners for the intro page
+startButton.addEventListener("click", toggleIntro);
 
 //Event listeners for the quiz pages
