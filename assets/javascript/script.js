0341f256adca8a021bf79e5de7224276080b49fd
Jonathan W <jwilliams12117@gmail.com>
2022-01-09 19:18:59 -0700

    Pseudocoded the structure of the script file

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+//Variables for the intro page logic
+
+
+//Variables for the quiz element logic
+
+//Function to add intro page
+
+//Function to add quiz elements to the page
+
+//Function to assess the correctness of a user answer
+
+//Function to decrement the time/score
+
+//Event listeners for the intro page
+
+//Event listeners for the quiz pages
