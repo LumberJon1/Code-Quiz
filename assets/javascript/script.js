48e7cfbe8512a391c423c0ca072646ee00694807
Jonathan W <jwilliams12117@gmail.com>
2022-01-09 20:32:41 -0700

    Added questions object to hold questions and answers, modified start function to toggle display of quiz questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,6 +5,87 @@ var introDescriptionEl = introSectionEl.querySelector("p");
 var startButton = introSectionEl.querySelector("button");
 
 //Variables for the quiz element logic
+var quizSectionEl = document.querySelector("#quiz-section");
+
+//Quiz questions object, which contains 15 question objects, each with a question string,
+//an answers array of strings, and a correct answer, which is an index of the answers array.
+var quizQuestions = {
+    question1: {
+        question: "What programming language is used to style webpages?",
+        answers: ["Java", "CSS", "Javascript", "HTML"],
+        correct: "CSS"
+    },
+    question2: {
+        question: "In the following array, how might you access the letter 'a'?\nvar array = ['a', 'b', 'c', 'd']",
+        answers: ["array.'a'", "array[1]", "array[0]", "array: a"],
+        correct: "array[0]"
+    },
+    question3: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question4: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question5: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question6: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question7: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question8: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question9: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question10: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question11: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question12: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question13: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question14: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+    question15: {
+        question: "",
+        answers: ["", "", "", ""],
+        correct: ""
+    },
+};
 
 //Function to toggle the display of the intro page
 var toggleIntro = function() {
@@ -16,13 +97,30 @@ var toggleIntro = function() {
     }
 }
 
+var startQuiz = function() {
+    //Toggle display of both the intro and the quiz sections
+    toggleIntro();
+    //I would like to use the toggle() function but for some reason it doesn't work on the first run.
+    quizSectionEl.style.display = "flex";
+
+    //Call the function that will loop through the quizQuestions object
+}
+
 //Function to add quiz elements to the page
+var toggleQuestions = function() {
+    if (quizSectionEl.style.display === "none") {
+        quizSectionEl.style.display = "flex";    
+    }
+    else {
+        quizSectionEl.style.display = "none";
+    }
+}
 
 //Function to assess the correctness of a user answer
 
 //Function to decrement the time/score
 
 //Event listeners for the intro page
-startButton.addEventListener("click", toggleIntro);
+startButton.addEventListener("click", startQuiz);
 
 //Event listeners for the quiz pages
