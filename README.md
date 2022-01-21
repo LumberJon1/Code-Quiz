66672650895c2fe035e3fabb0bdb877e45e5fc58
Jonathan W <jwilliams12117@gmail.com>
2022-01-21 15:58:42 -0700

    Reactivated all questions and updated README

--- README.md
+++ README.md
@@ -10,23 +10,10 @@ Once all questions are answered or timer reaches 0, the score will be displayed,
 any previous high scores.  The quiz will allow users to enter their name if a high score is
 achieved.
 
-## Acceptance Criteria:
-GIVEN I am taking a code quiz...
+## Screenshot:
+![screenshot](./assets/images/screenshot.JPG)
 
-* WHEN I click the start button
-*   THEN a timer starts and I am presented with a question
-
-* WHEN I answer a question
-*   THEN I am presented with another question
-
-* WHEN I answer a question incorrectly
-*   THEN time is subtracted from the clock
-
-* WHEN all questions are answered or the timer reaches 0
-*   THEN the game is over
-
-* WHEN the game is over
-*   THEN I can save my initials and score
+## Deployed Link:
 
 ## Author:
 Jonathan Williams
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -46,72 +46,72 @@ var quizQuestions = [
         answers: ["array.push()", "array.pushItem()", "array.append()", "array.add()"],
         correct: "array.push()"
     },
-    // question5 = {
-    //     number: 5,
-    //     question: "What method makes a JSON object readable by localStorage?",
-    //     answers: ["JASON.stringify()", "JSON.readable()", "JSON.stringify()", "JSON.parse()"],
-    //     correct: "JSON.stringify()"
-    // },
-    // question6 = {
-    //     number: 6,
-    //     question: "Which of the following operators is strict comparison?",
-    //     answers: ["=", "==", "===", "!="],
-    //     correct: "==="
-    // },
-    // question7 = {
-    //     number: 7,
-    //     question: "What would the following code evaluate to?\nif (0 < 5 || 0 > 50) {};",
-    //     answers: ["true", "false", "True", "False"],
-    //     correct: "true"
-    // },
-    // question8 = {
-    //     number: 8,
-    //     question: "Which attribute should every <img> element have?",
-    //     answers: ["background-position", "img", "alt", "id"],
-    //     correct: "alt"
-    // },
-    // question9 = {
-    //     number: 9,
-    //     question: "In a CSS stylesheet, what kind of selector is represented by '.'?",
-    //     answers: ["element", "class", "universal", "id"],
-    //     correct: "class"
-    // },
-    // question10 = {
-    //     number: 10,
-    //     question: "How would one access the 'number' attribute of an object named 'question'?",
-    //     answers: ["question[number]", "question.number()", "question.number", "question(number)"],
-    //     correct: "question.number"
-    // },
-    // question11 = {
-    //     number: 11,
-    //     question: "What method will allow form submission on a webpage to be handled by JavaScript?",
-    //     answers: [".preventDefault()", ".allow()", ".javaHandler()", ".formAllow()"],
-    //     correct: ".preventDefault()"
-    // },
-    // question12 = {
-    //     number: 12,
-    //     question: "What does the && comparison operator do?",
-    //     answers: ["Checks whether two expressions are both true", "Checks whether two expressions are both false", "Checks whether two expressions are strictly equal", "Checks whether either of two expressions are true"],
-    //     correct: "Checks whether two expressions are both true"
-    // },
-    // question13 = {
-    //     number: 13,
-    //     question: "Which git command creates and checks out into a new branch?",
-    //     answers: ["git checkout new", "git checkout -b", "git branch checkout", "git checkout new branch"],
-    //     correct: "git checkout -b"
-    // },
-    // question14 = {
-    //     number: 14,
-    //     question: "What is the syntax to listen for a click event and call the runCommand() function?",
-    //     answers: ["addEventListener(click, runCommand)", "addEventListener('click', runCommand)", "addEventListener('click', runCommand()", "addEventListener('click', runCommand())"],
-    //     correct: "addEventListener('click', runCommand())"
-    // },
-    // question15 = {
-    //     number: 15,
-    //     question: "Which of the following operators would return false? \nif(0 ___ 0) {};",
-    //     answers: ["==", "===", "<=", "!="],
-    //     correct: "!="
-    // },
+    question5 = {
+        number: 5,
+        question: "What method makes a JSON object readable by localStorage?",
+        answers: ["JASON.stringify()", "JSON.readable()", "JSON.stringify()", "JSON.parse()"],
+        correct: "JSON.stringify()"
+    },
+    question6 = {
+        number: 6,
+        question: "Which of the following operators is strict comparison?",
+        answers: ["=", "==", "===", "!="],
+        correct: "==="
+    },
+    question7 = {
+        number: 7,
+        question: "What would the following code evaluate to?\nif (0 < 5 || 0 > 50) {};",
+        answers: ["true", "false", "True", "False"],
+        correct: "true"
+    },
+    question8 = {
+        number: 8,
+        question: "Which attribute should every <img> element have?",
+        answers: ["background-position", "img", "alt", "id"],
+        correct: "alt"
+    },
+    question9 = {
+        number: 9,
+        question: "In a CSS stylesheet, what kind of selector is represented by '.'?",
+        answers: ["element", "class", "universal", "id"],
+        correct: "class"
+    },
+    question10 = {
+        number: 10,
+        question: "How would one access the 'number' attribute of an object named 'question'?",
+        answers: ["question[number]", "question.number()", "question.number", "question(number)"],
+        correct: "question.number"
+    },
+    question11 = {
+        number: 11,
+        question: "What method will allow form submission on a webpage to be handled by JavaScript?",
+        answers: [".preventDefault()", ".allow()", ".javaHandler()", ".formAllow()"],
+        correct: ".preventDefault()"
+    },
+    question12 = {
+        number: 12,
+        question: "What does the && comparison operator do?",
+        answers: ["Checks whether two expressions are both true", "Checks whether two expressions are both false", "Checks whether two expressions are strictly equal", "Checks whether either of two expressions are true"],
+        correct: "Checks whether two expressions are both true"
+    },
+    question13 = {
+        number: 13,
+        question: "Which git command creates and checks out into a new branch?",
+        answers: ["git checkout new", "git checkout -b", "git branch checkout", "git checkout new branch"],
+        correct: "git checkout -b"
+    },
+    question14 = {
+        number: 14,
+        question: "What is the syntax to listen for a click event and call the runCommand() function?",
+        answers: ["addEventListener(click, runCommand)", "addEventListener('click', runCommand)", "addEventListener('click', runCommand()", "addEventListener('click', runCommand())"],
+        correct: "addEventListener('click', runCommand())"
+    },
+    question15 = {
+        number: 15,
+        question: "Which of the following operators would return false? \nif(0 ___ 0) {};",
+        answers: ["==", "===", "<=", "!="],
+        correct: "!="
+    },
 ];
 
 //Function to toggle the display of the intro page
@@ -137,7 +137,7 @@ var startQuiz = function() {
     quizSectionEl.style.display = "flex";
 
     //Set timer
-    timeValue = 10;
+    timeValue = 100;
     
     setInterval(function() {
         timerEl.textContent = "Time: "+timeValue;
